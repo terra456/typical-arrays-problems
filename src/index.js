9b0cdea341ef761622da689105868ce97c27684a
Olga Bainova (nb) <terra.456@yandex.ru>
2020-02-20 00:22:28 +0500

    feat: implement 3 functions for searching minimal value, maximal value, and an average value of a given array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        for (var i = 0; i < array.length; i++)
+        {
+            var minValue = array[i];
+            for (var j = 0; j < array.length; j++) {
+                if (array[j] < minValue) {
+                    minValue = array[j];
+                }
+            }   
+        }
+        return minValue;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        for (var i=0; i<array.length; i++) {
+            var maxValue = array[i];
+            for (var j=0; j<array.length; j++) {
+                if (array[j] > maxValue) {
+                    maxValue = array[j]
+                }
+            }
+        }
+        return maxValue;
+    }
+    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        var arraySum = 0;
+        for (i=0; i<array.length; i++) {
+            arraySum = arraySum + array[i];
+        }
+        var averageValue = arraySum / array.length;
+        return averageValue;
+    }
+  
 }
+
